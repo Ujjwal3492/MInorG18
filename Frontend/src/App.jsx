5383ef7b64a6fadb9175cbf6953469da864ad28d
AnanymousCtrl <76686931+AnanymousCtrl@users.noreply.github.com>
2024-12-05 23:02:18 +0530

    remaining commits

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,20 @@
+import Card from "./Components/EVENTS/Card.jsx"
 import Hero from "./Components/Heros/Hero.jsx"
 import Navbar from "./Components/Navbar/Navbar.jsx"
 
-
+/**
+ * The main application component.
+ * 
+ * @returns {JSX.Element} The JSX element representing the application.
+ */
 function App() {
-  
-
   return (
     <>
-     <Navbar/>
-     <Hero/>
+      <Navbar />
+      <Hero />
+      <Card />
     </>
   )
 }
 
-export default App
+export default App;
